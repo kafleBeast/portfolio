3a516007387cfa80e44e119f1c7e455c1c810b69
kafleBeast <kafleBeast@users.noreply.github.com>
2021-08-30 12:10:41 +0000

    Deploying to gh-pages from  @ cdafae7715858e5f926424936ac4db955342dde3 🚀

---
+++ precache-manifest.4ee7237e7d26d1336d1eaf8edbdf3d5e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9c0cf1ed17e292aba656ae84b55480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1de4cadeab93aac520fe",
+    "url": "/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "bd2ab8ead97f11dffadd",
+    "url": "/static/css/main.165e2119.chunk.css"
+  },
+  {
+    "revision": "023ddaaab6f182ab4e20",
+    "url": "/static/js/2.ce2c4ad5.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.ce2c4ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de4cadeab93aac520fe",
+    "url": "/static/js/3.d8f14085.chunk.js"
+  },
+  {
+    "revision": "bd2ab8ead97f11dffadd",
+    "url": "/static/js/main.dee263f5.chunk.js"
+  },
+  {
+    "revision": "b560ec52fdbdb5d72f28",
+    "url": "/static/js/runtime-main.6b87ec53.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e32563048b93117c5cfe026de5c1ec2e",
+    "url": "/static/media/greLandingPage.e3256304.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b1051c021206d73e59b4e9a1be0177b8",
+    "url": "/static/media/siteUnderConstruction.b1051c02.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "be39949d68b59c479c18a18c827e89a6",
+    "url": "/static/media/stonybrook.be39949d.png"
+  }
+]);
